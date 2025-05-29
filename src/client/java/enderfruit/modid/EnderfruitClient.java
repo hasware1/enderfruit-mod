f2618fa16e88ef0917953d14f784ee45e74da37f
hasware1 <naughtybrothers11@gmail.com>
2025-05-30 01:40:59 +0500

    initial commit

---
+++ src/client/java/enderfruit/modid/EnderfruitClient.java
@@ -0,0 +1,10 @@
+package enderfruit.modid;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EnderfruitClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
